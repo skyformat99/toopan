091ed02e3e5c2dc5129dae361fdcb68199516f85
依然夏末 <54070326+yiranxiamo@users.noreply.github.com>
2020-11-19 02:05:11 +0800

    Update en-header.php

--- includes/en-header.php
+++ includes/en-header.php
@@ -34,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="./"><?php echo $conf['title']?></a>
+          <a class="navbar-brand" href="./"><?php echo $conf['en_title']?></a>
         </div>
         <div class="navbar-collapse collapse navbar-responsive-collapse">
           <ul class="nav navbar-nav">
